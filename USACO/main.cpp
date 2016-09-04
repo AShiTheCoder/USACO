beb95216ce7d354018de342a23312ec9bab22a38
Andrew Shi <ashilol95@gmail.com>
2016-09-04 15:39:26 -0700

    Initial Commit

---
+++ USACO/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  USACO
+//
+//  Created by Andrew Shi on 9/4/16.
+//  Copyright © 2016 Andrew Shi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
