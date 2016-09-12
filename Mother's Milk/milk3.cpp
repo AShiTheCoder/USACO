410be9f305c69cd51e36ce54be2984a7906a392c
Andrew Shi <ashilol95@gmail.com>
2016-09-11 22:54:02 -0700

    started Mother's Milk

---
+++ Mother's Milk/milk3.cpp
@@ -0,0 +1,17 @@
+/*
+ID: ashilol1
+PROG: milk3
+LANG: C++
+*/
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    ifstream in ("/Users/AShi/Documents/Repos/USACO/Mother's Milk/milk3.txt");
+//    ifstream in ("milk3.in");
+//    ofstream out ("milk3.out");
+    
+    return 0;
+}
